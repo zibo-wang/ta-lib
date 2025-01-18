4276685c9b8e1bded650214addb86d3d497e9f2a
Zibo Wang <zibo.w@outlook.com>
2025-01-18 23:43:55 +1100

    remove old build system

--- cmake/toolchain-linux-arm64.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
-
-set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
--- cmake/toolchain-linux-i386.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR x86)
-
-set(CMAKE_C_COMPILER i686-linux-gnu-gcc)
